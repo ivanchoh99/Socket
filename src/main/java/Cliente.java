8f5258f4a7984efa35069ed27279f3e134a3e526
Ivan H CArvajal Acevedo <ivanchoh99@gmail.com>
2023-04-26 10:20:50 -0500

    Correccion de conexion quede abierta con el cliente

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -8,33 +8,39 @@ public class Cliente {
 
     public static void main(String[] args) throws IOException {
 
-    Socket socket;
-    DataInputStream input;
-    DataOutputStream output;
+        Socket socket;
+        DataInputStream input;
+        DataOutputStream output;
 
         String host = "192.168.1.5";
         int port = 5000;
+        String solicitud = "Si desea salir enviar 'x' \n ¿ Numero de telefono que desea buscar ?";
+
 
         socket = new Socket(host, port);
 
         input = new DataInputStream(socket.getInputStream());
         output = new DataOutputStream(socket.getOutputStream());
 
-        Scanner in = new Scanner(System.in);
-        String numeroTel = null;
+        while (true) {
 
+            System.out.println(solicitud);
 
-            String mensaje  = input.readUTF();
-            System.out.println(mensaje);
+            Scanner in = new Scanner(System.in);
+            String numeroTel = null;
 
             numeroTel = in.next();
+
             output.writeUTF(numeroTel);
 
-        System.out.println("---------------------------------------");
-        System.out.println(input.readUTF());
-        System.out.println("---------------------------------------");
-        System.out.println(input.readUTF());
-
+            if(numeroTel.equals("x")){
+                System.out.println("ADIOS");
+                break;
+            }else {
+                System.out.println("---------------------------------------");
+                System.out.println(input.readUTF());
+                System.out.println("---------------------------------------");
+            }
+        }
     }
-
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,74 +8,74 @@ import java.sql.*;
 public class Server {
     public static void main(String[] args) throws IOException, SQLException {
 
-        final int port = 5000;
 
-
-        String solicitud = "¿ Numero de telefono que desea buscar ?";
-        String respuesta = null;
-
+//    Conexion Base de Datos y utilidades SQL
         String url = "jdbc:mysql://localhost:3306/poli";
         String username = "root";
         String password = "michi0602";
-
+        ResultSet resultSet = null;
         Connection con = DriverManager.getConnection(url, username, password);
 
-        ResultSet resultSet = null;
+//      Variables String
+        String respuesta = "";
 
-
+//        Configuracion variables serverSocket
+        final int port = 5000;
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socketCliente = null;
 
 
         System.out.println("Servidro Iniciado");
         while (true) {
+
             socketCliente = serverSocket.accept();
             System.out.println("Cliente conectado");
 
             DataInputStream input = new DataInputStream(socketCliente.getInputStream());
             DataOutputStream output = new DataOutputStream(socketCliente.getOutputStream());
 
-            output.writeUTF(solicitud);
-            respuesta = input.readUTF();
+            while (true) {
 
-            String sql = "select p.tel_persona, p.nombre_persona , p.direc_persona, c.nombre_ciudad  from persona p inner join  ciudad c on c.id_ciudad = p.id_cidudad_persona where p.tel_persona  = '"+respuesta+"'";
+                respuesta = input.readUTF();
+                System.out.println("Cliente: " + respuesta);
 
-            Statement statement = con.createStatement();
+                if(respuesta.equals("x")) {
+                    System.out.println("Cliente desconectado");
+                    socketCliente.close();
+                    break;
+                }
 
-            resultSet = statement.executeQuery(sql);
+                String sql = "select p.tel_persona, p.nombre_persona , p.direc_persona, c.nombre_ciudad  from persona p inner join  ciudad c on c.id_ciudad = p.id_cidudad_persona where p.tel_persona  = '" + respuesta + "'";
+                Statement statement = con.createStatement();
+                resultSet = statement.executeQuery(sql);
+                String respuestaConsulta = null;
 
-            String respuestaConsulta = null;
+                if (resultSet.next()) {
+                    String tel = resultSet.getString("tel_persona");
+                    String nom = resultSet.getString("nombre_persona");
+                    String direc = resultSet.getString("direc_persona");
+                    String ciudad = resultSet.getString("nombre_ciudad");
 
-            if(resultSet.next()) {
-                String tel = resultSet.getString("tel_persona");
-                String nom = resultSet.getString("nombre_persona");
-                String direc = resultSet.getString("direc_persona");
-                String ciudad = resultSet.getString("nombre_ciudad");
-
-                System.out.println("---------------------------------------");
-                respuestaConsulta = "Telefono: " + tel + "\n" +
-                        "Nombre: " + nom + "\n" +
-                        "Direccion: " + direc + "\n" +
-                        "Ciudad: " + ciudad;
-                System.out.println("---------------------------------------");
+                    System.out.println("---------------------------------------");
+                    respuestaConsulta = "Telefono: " + tel + "\n" +
+                            "Nombre: " + nom + "\n" +
+                            "Direccion: " + direc + "\n" +
+                            "Ciudad: " + ciudad;
+                    System.out.println("---------------------------------------");
 
 
-                System.out.println(respuestaConsulta);
-                output.writeUTF(respuestaConsulta);
-                output.writeUTF("ADIOS");
-                socketCliente.close();
-                System.out.println("Cliente desconectado");
-            }else{
-                System.out.println("---------------------------------------");
-                respuestaConsulta = "La persona con ese numero de telefono no existe";
-                System.out.println("La persona con ese numero de telefono no existe");
-                output.writeUTF(respuestaConsulta);
-                System.out.println("---------------------------------------");
-                output.writeUTF("ADIOS");
-                socketCliente.close();
-                System.out.println("Cliente desconectado");
+                    System.out.println(respuestaConsulta);
+                    output.writeUTF(respuestaConsulta);
+//                socketCliente.close();
+//                System.out.println("Cliente desconectado");
+                } else {
+                    System.out.println("---------------------------------------");
+                    respuestaConsulta = "La persona con ese numero de telefono no existe";
+                    System.out.println(respuestaConsulta);
+                    output.writeUTF(respuestaConsulta);
+                    System.out.println("---------------------------------------");
+                }
             }
-
 
 
         }
